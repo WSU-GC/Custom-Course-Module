760181fd1ee3b3a6bbe3b944c678d3b2f594dd58
dworthen <worthend.derek@gmail.com>
2014-11-13 18:56:59 -0800

    Create Groups when merging

--- src/edu/wsu/CourseManagement.java
+++ src/edu/wsu/CourseManagement.java
@@ -1,12 +1,22 @@
 package edu.wsu;
 
+import java.util.List;
+
+import blackboard.base.FormattedText;
 import blackboard.data.ValidationException;
 import blackboard.data.course.Course;
 import blackboard.data.course.CourseCourseManager;
 import blackboard.data.course.CourseCourseManagerFactory;
+import blackboard.data.course.CourseMembership;
+import blackboard.data.course.Group;
+import blackboard.data.course.GroupMembership;
+import blackboard.persist.KeyNotFoundException;
 import blackboard.persist.PersistenceException;
 import blackboard.persist.course.CourseDbLoader;
 import blackboard.persist.course.CourseDbPersister;
+import blackboard.persist.course.GroupDbLoader;
+import blackboard.persist.course.GroupDbPersister;
+import blackboard.persist.course.GroupMembershipDbPersister;
 
 
 public class CourseManagement {
@@ -69,6 +79,7 @@ public class CourseManagement {
 		for (int i = 0, l = childIds.length; i < l; i++) {
 			Course childCourse = courseLoader.loadByCourseId(childIds[i]);
 			ccManager.addChildToMaster(childCourse.getId(), parentCourse.getId());
+			CourseManagement.createGroup(parentId, childIds[i]);
 		}
 		
 	}
@@ -80,6 +91,56 @@ public class CourseManagement {
 		Course childCourse = courseLoader.loadByCourseId(childId);
 		ccManager.removeChildFromMaster(childCourse.getId(), parentCourse.getId(), 
 				CourseCourseManager.DecrosslistStyle.KEEP_ORIGINAL_COURSE);
+	}
+	
+	public static void createGroup(String parentCourseId, String childCourseId) 
+			throws KeyNotFoundException, PersistenceException, ValidationException {
+		CourseDbLoader courseLoader = CourseDbLoader.Default.getInstance();
+		GroupDbLoader groupLoader = GroupDbLoader.Default.getInstance();
+		GroupDbPersister groupPersister = GroupDbPersister.Default.getInstance();
+		GroupMembershipDbPersister gmPersister = GroupMembershipDbPersister.Default.getInstance();
+		
+		Course parentCourse = courseLoader.loadByCourseId(parentCourseId);
+		Course childCourse = courseLoader.loadByCourseId(childCourseId);
+		CourseWrapper childWrapper = new CourseWrapper(childCourse);
+		List<CourseMembership> childMemberships = childWrapper.loadMemberships();
+		
+		List<Group> groups = groupLoader.loadGroupsAndSetsByCourseId(parentCourse.getId());
+		
+		boolean doesCurChildGroupExist = false;
+		Group newGroup = new Group();
+		for(int k=0, n = groups.size(); k < n; k++) { // DOES GROUP EXIST?
+				
+			Group curGroup = groups.get(k);
+			if (curGroup.getTitle().equalsIgnoreCase(childCourseId)) {
+				newGroup = groupLoader.loadById(curGroup.getId());
+				doesCurChildGroupExist = true;
+				break; 
+			}
+			
+		}
+		
+		if (!doesCurChildGroupExist) { // IF NOT THEN CREATE ONE
+			newGroup.setTitle(childCourseId);
+			FormattedText text = new FormattedText(childCourseId, FormattedText.Type.PLAIN_TEXT);
+			newGroup.setDescription(text);
+			newGroup.setCourseId(parentCourse.getId());
+			newGroup.setIsAvailable(false);
+			groupPersister.persist(newGroup);
+		}
+		
+		for(int j=0, m = childMemberships.size(); j < m; j++) {
+			
+			CourseMembership curMembership = childMemberships.get(j);
+			
+			if(!newGroup.isUserAMember(curMembership.getId())) {
+				GroupMembership groupMembership = new GroupMembership();
+				groupMembership.setCourseMembershipId(curMembership.getId());
+				groupMembership.setGroupId(newGroup.getId());
+				gmPersister.persist(groupMembership);
+			}
+		}  
+		
 	}
 	
 }
